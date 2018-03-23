e7775522f1dc3cb0b85e9874fcdfeeb80b802487
danhuman <info@danhillman.co.uk>
2018-03-23 16:03:49 +0000

    update 2

--- js/index.20180323160127.js
+++ js/index.20180323160127.js
@@ -6,26 +6,26 @@ var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw
 $(document).ready(function() {
 r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=960){$('.js').attr('src', (dpi>1) ? 'images/rings-black-1050.png' : 'images/rings-black-525.png');
 $('.js-2').attr('src', (dpi>1) ? 'images/bch-sec-text-1468.png' : 'images/bch-sec-text-734.png');
-$('.js-3').attr('src', (dpi>1) ? 'images/rings-black-1050-2.png' : 'images/rings-black-525-2.png');
-$('.js-4').attr('src', (dpi>1) ? 'images/bch-sec-text-1468-2.png' : 'images/bch-sec-text-734-2.png');
-$('.js-5').attr('src', (dpi>1) ? 'images/rings-black-1050-3.png' : 'images/rings-black-525-3.png');
-$('.js-6').attr('src', (dpi>1) ? 'images/bch-sec-text-1468-3.png' : 'images/bch-sec-text-734-3.png');
+$('.js-3').attr('src', (dpi>1) ? 'images/rings-black-1050.png' : 'images/rings-black-525.png');
+$('.js-4').attr('src', (dpi>1) ? 'images/bch-sec-text-1468.png' : 'images/bch-sec-text-734.png');
+$('.js-5').attr('src', (dpi>1) ? 'images/rings-black-1050.png' : 'images/rings-black-525.png');
+$('.js-6').attr('src', (dpi>1) ? 'images/bch-sec-text-1468.png' : 'images/bch-sec-text-734.png');
 $('.js-7').attr('src', (dpi>1) ? 'images/bch-dnb-text-1468.png' : 'images/bch-dnb-text-734.png');
-$('.js-8').attr('src', (dpi>1) ? 'images/bch-dnb-text-1468-2.png' : 'images/bch-dnb-text-734-2.png');
-$('.js-9').attr('src', (dpi>1) ? 'images/bch-dnb-text-1468-3.png' : 'images/bch-dnb-text-734-3.png');
+$('.js-8').attr('src', (dpi>1) ? 'images/bch-dnb-text-1468.png' : 'images/bch-dnb-text-734.png');
+$('.js-9').attr('src', (dpi>1) ? 'images/bch-dnb-text-1468.png' : 'images/bch-dnb-text-734.png');
 $('.js-10').attr('src', (dpi>1) ? 'images/rings-white-1050.png' : 'images/rings-white-525.png');
-$('.js-11').attr('src', (dpi>1) ? 'images/rings-white-1050-2.png' : 'images/rings-white-525-2.png');
-$('.js-12').attr('src', (dpi>1) ? 'images/rings-white-1050-3.png' : 'images/rings-white-525-3.png');}else{$('.js').attr('src', (dpi>1) ? 'images/rings-black-200.png' : 'images/rings-black-100.png');
+$('.js-11').attr('src', (dpi>1) ? 'images/rings-white-1050.png' : 'images/rings-white-525.png');
+$('.js-12').attr('src', (dpi>1) ? 'images/rings-white-1050.png' : 'images/rings-white-525.png');}else{$('.js').attr('src', (dpi>1) ? 'images/rings-black-200.png' : 'images/rings-black-100.png');
 $('.js-2').attr('src', (dpi>1) ? 'images/bch-sec-text-200.png' : 'images/bch-sec-text-100.png');
-$('.js-3').attr('src', (dpi>1) ? 'images/rings-black-200-2.png' : 'images/rings-black-100-2.png');
-$('.js-4').attr('src', (dpi>1) ? 'images/bch-sec-text-200-2.png' : 'images/bch-sec-text-100-2.png');
+$('.js-3').attr('src', (dpi>1) ? 'images/rings-black-200.png' : 'images/rings-black-100.png');
+$('.js-4').attr('src', (dpi>1) ? 'images/bch-sec-text-200.png' : 'images/bch-sec-text-100.png');
 $('.js-5').attr('src', (dpi>1) ? 'images/rings-black-350.png' : 'images/rings-black-175.png');
 $('.js-6').attr('src', (dpi>1) ? 'images/bch-sec-text-488.png' : 'images/bch-sec-text-244.png');
 $('.js-7').attr('src', (dpi>1) ? 'images/bch-dnb-text-490.png' : 'images/bch-dnb-text-245.png');
 $('.js-8').attr('src', (dpi>1) ? 'images/bch-dnb-text-200.png' : 'images/bch-dnb-text-100.png');
-$('.js-9').attr('src', (dpi>1) ? 'images/bch-dnb-text-200-2.png' : 'images/bch-dnb-text-100-2.png');
+$('.js-9').attr('src', (dpi>1) ? 'images/bch-dnb-text-200.png' : 'images/bch-dnb-text-100.png');
 $('.js-10').attr('src', (dpi>1) ? 'images/rings-white-200.png' : 'images/rings-white-100.png');
-$('.js-11').attr('src', (dpi>1) ? 'images/rings-white-200-2.png' : 'images/rings-white-100-2.png');
+$('.js-11').attr('src', (dpi>1) ? 'images/rings-white-200.png' : 'images/rings-white-100.png');
 $('.js-12').attr('src', (dpi>1) ? 'images/rings-white-350.png' : 'images/rings-white-175.png');}}};
 if(!window.HTMLPictureElement){$(window).resize(r);r();}
 (function(){$('a[href^="#"]').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
